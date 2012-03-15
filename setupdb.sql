e98c6bfdeec5ca0856132633e463e90a59d1601b
Misha Reyzlin <hello@mishareyzlin.com>
2012-03-15 02:34:10 +0100

    drop db before creating it

--- setupdb.sql
+++ setupdb.sql
@@ -1,3 +1,4 @@
+drop database publicspaceinvaders_db;
 create database publicspaceinvaders_db;
 grant usage on *.* to psi_admin@localhost identified by 'Bla123';
 grant all privileges on publicspaceinvaders_db.* to psi_admin@localhost;
